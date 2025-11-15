dcdebe63821f49f39a49e31dbb3fc54a29ed1070
kelton18 <notlek05@gmail.com>
2025-11-15 16:27:35 -0700

    added fade-in to images when scrolling, added toggle-theme button.

---
+++ Portfolio.js
@@ -0,0 +1,35 @@
+const images = document.querySelectorAll("img.hidden");
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fade-in");
+            entry.target.classList.remove("hidden");
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 }); 
+
+images.forEach(img => {
+    observer.observe(img);
+})
+
+const toggleBtn = document.getElementById("theme-toggle");
+
+// 1. on load, apply saved theme (if any)
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode")
+}
+
+// 2. When button is clicked, toggle + save
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
